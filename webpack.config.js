fb7a99c9b5408f75508e4fcabbdfd3b5067e64d4
Am-Coder <mishraaman2210@gmail.com>
2022-01-30 22:05:19 +0530

    fixes: webpack-5 Buffer polyfill issue resolved

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,12 @@ module.exports = {
                 }
             }
         ]
-    }
-
+    },
+    plugins: [
+        // Work around for Buffer is undefined:
+        // https://github.com/webpack/changelog-v5/issues/10
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        })
+    ]
 };
