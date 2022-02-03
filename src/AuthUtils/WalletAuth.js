d463b6b7db70b628dd087f719400c5870cc1505f
Am-Coder <mishraaman2210@gmail.com>
2022-02-03 22:54:57 +0530

    changes: Improvements in wallet-connect and existing sessions

--- src/AuthUtils/WalletAuth.js
+++ src/AuthUtils/WalletAuth.js
@@ -5,13 +5,14 @@ import axios, { AxiosResponse, AxiosError } from 'axios';
 import * as constants from "../constants"
 
 let ONEC_AUTH_USERS_BASE_URL = constants.ONEC_NAAS_BASE_URL;
-
-let current
-let _activeAccount = null
-let _chainId = null
-let _user = null
-let _wcConnector = null
-let _authMethod = null
+let _mmConnector = null;
+let _wcInitialized = false;
+let _activeAccount = null;
+let _chainId = null;
+let _user = null;
+let _wcConnector = null;
+let _authMethod = null;
+let _sessionId = null;
 
 /**
  * 
@@ -22,55 +23,51 @@ let _authMethod = null
 function ErrorHandler(error, reject) {
     let msg = '';
     if (error.code === 4001) {
-        // Request rejected. Please accept to continue.
         msg = 'Request rejected. Please accept to continue.';
     } else if (error.code === 4100) {
-        // Account is inaccessible. Please unlock your account.
         msg = 'Account is inaccessible. Please unlock your account.';
     } else {
         msg = 'error: ' + error.message;
     }
     console.error(msg);
-    reject(new Error(msg));
+    return reject(new Error(msg));
 }
 
 function connectWC() {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         if (!_wcConnector) {
             // Create a connector
             _wcConnector = new WalletConnect({
-                bridge: "https://bridge.walletconnect.org", // Required
+                bridge: 'https://bridge.walletconnect.org', // Required
                 qrcodeModal: QRCodeModal,
             });
             if (!_wcConnector)
-                throw new Error("Could not instantiate WalletConnect");
+                return reject(new Error('Could not instantiate WalletConnect'));
 
             // Subscribe to connection events
-            _wcConnector.on("connect", (error, payload) => {
+            _wcConnector.on('connect', (error, payload) => {
                 if (error) {
-                    throw error;
+                    return reject(error);
                 }
 
                 // Get provided accounts and chainId
                 _activeAccount = payload.params[0].accounts[0];
                 _chainId = payload.params[0].chainId;
-                console.log("connect", payload.params);
             });
 
-            _wcConnector.on("session_update", (error, payload) => {
+            _wcConnector.on('session_update', (error, payload) => {
                 if (error) {
-                    throw error;
+                    return reject(error);
                 }
 
                 // Get updated accounts and chainId
                 _activeAccount = payload.params[0].accounts[0];
                 _chainId = payload.params[0].chainId;
-                console.log("session", payload.params);
             });
 
-            _wcConnector.on("disconnect", (error, payload) => {
+            _wcConnector.on('disconnect', (error, payload) => {
                 if (error) {
-                    throw error;
+                    return reject(error);
                 }
 
                 // Delete connector
@@ -78,103 +75,119 @@ function connectWC() {
                 _chainId = null
                 _wcConnector = null
                 _activeAccount = null
-                axios.post(ONEC_AUTH_USERS_BASE_URL + 'logout/' + _activeAccount + '/');
-                console.log("disconnect", payload.params);
             });
         }
 
-        // if (!_activeAccount)
-        //     _wcConnector.disconnect();
+        if (!_wcInitialized && _wcConnector.connected)
+            await _wcConnector.updateSession({ chainId: _wcConnector.chainId, accounts: _wcConnector.accounts });
 
         // Check if connection is already established
         if (!_wcConnector.connected) {
             // create new session
-            resolve(_wcConnector.connect());
-            return;
+            _wcConnector.connect().then(() => { resolve(_activeAccount) }).catch((error) => { reject(error) });
+        } else {
+            return resolve(_activeAccount);
         }
-        resolve();
     });
 }
 
-function toHex(s) {
-    // utf8 to latin1
-    var s = unescape(encodeURIComponent(s))
-    var h = ''
-    for (var i = 0; i < s.length; i++) {
-        h += s.charCodeAt(i).toString(16)
+function isLoggedIn() {
+    if (_user && _sessionId && _activeAccount && _authMethod != null) {
+        return true;
+    } else {
+        _user = null; _sessionId = null; _activeAccount = null; _authMethod = null;
+        return false;
     }
-    return h
 }
 
 function userLogin(resolve, reject) {
-    return axios.post(ONEC_AUTH_USERS_BASE_URL + 'init/' + _activeAccount + '/')
+    axios.post(ONEC_AUTH_USERS_BASE_URL + 'init/' + _activeAccount + '/')
         .then((response) => {
             if (response.status >= 200 && response.status < 300) {
                 let message = response.data.message;
-                if (_authMethod === "metamask")
+                _sessionId = response.data.session_id;
+                if (_authMethod === 'metamask')
                     return window.ethereum.request({
                         method: 'personal_sign',
                         params: [message, window.ethereum.selectedAddress]
                     });
-                else if (_authMethod == "walletconnect") {
-                    return _wcConnector.signPersonalMessage([toHex(message), _activeAccount]);
+                else if (_authMethod == 'walletconnect') {
+                    return _wcConnector.signPersonalMessage(['' + message, _activeAccount]);
                 } else
-                    throw new Error("Invalid auth attempt faced");
+                    return reject(new Error('Invalid auth attempt faced'));
             } else {
-                throw new Error("Error in creating user session");
+                return reject(new Error('Error in creating user session'));
             }
-        }).then((sig) => {
+        }).then((_signature) => {
             // do user auth with signature on the session message
             return axios.post(ONEC_AUTH_USERS_BASE_URL + 'auth/' + _activeAccount + '/', {
-                signature: sig
+                signature: _signature,
+                session_id: _sessionId
             });
         }).then((response) => {
             if (response.status === 200) {
                 console.log('User authenticated');
                 _user = { user: response.data.user, token: response.data.token };
-                resolve(_user);
+                return resolve(_user);
             } else if (response.status >= 400 && response.status < 500) {
                 // User is not authenticated
                 console.log('Something went wrong');
-                throw new Error(response.status + ': ' + response.statusText);
+                reject(new Error(response.status + ': ' + response.statusText));
             } else if (response.status >= 500) {
                 console.log('Server error');
-                throw new Error(response.status + ': ' + response.statusText);
+                reject(new Error(response.status + ': ' + response.statusText));
             }
-        });
+        }).catch((error) => { reject(error) });
 }
 
 export function withWalletConnect() {
     return new Promise((resolve, reject) => {
-        _authMethod = "walletconnect";
-        connectWC().then((response) => {
+        if (isLoggedIn()) {
+            if (_authMethod === 'walletconnect') {
+                return resolve(_user);
+            } else {
+                console.warn('Overriding existing user session with new walletconnect session');
+                _user = null; _sessionId = null; _activeAccount = null; _authMethod = null;
+            }
+        }
+        _authMethod = 'walletconnect';
+        connectWC().then(async (response) => {
             console.log(response);
-            userLogin(resolve, reject).catch((error) => { reject(error) });
+            _wcInitialized = true;
+            if (!response) {
+                _activeAccount = (await _wcConnector.sendCustomRequest({ method: 'eth_accounts' }))[0];
+                _chainId = await _wcConnector.sendCustomRequest({ method: 'eth_chainId' });
+            }
+            userLogin(resolve, reject);
         }).catch(error => { ErrorHandler(error, reject) });
     });
 }
 
 export function withMetamask() {
-    if (!current) {
-        current = new MetaMaskOnboarding();
-        // window.ethereum.on('accountsChanged', function(accounts) {
-        //     console.log('accountsChanged', accounts);
-        // });
-    }
-
-    if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
-        console.log('MetaMask is not installed');
-        current.startOnboarding();
-        return;
-    }
-
     return new Promise((resolve, reject) => {
-        _authMethod = "metamask";
+        if (isLoggedIn()) {
+            if (_authMethod === 'metamask') {
+                return resolve(_user);
+            } else {
+                console.warn('Overriding existing user session with new metamask session');
+                _user = null; _sessionId = null; _activeAccount = null; _authMethod = null;
+            }
+        }
+        if (!_mmConnector)
+            _mmConnector = new MetaMaskOnboarding();
+
+        if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+            console.log('MetaMask is not installed');
+            _mmConnector.startOnboarding();
+            return resolve(null);
+        }
+
+        _authMethod = 'metamask';
         window.ethereum.request({ method: 'eth_requestAccounts' })
-            .then(async (accounts) => {
+            .then((accounts) => {
                 console.log('accounts', accounts);
                 _activeAccount = accounts[0];
-                userLogin(resolve, reject).catch((error) => { reject(error) });
+                userLogin(resolve, reject);
             }).catch((error) => { ErrorHandler(error, reject) });
     });
-} 
+}
