67a7329d819dd9a03a8e3f56e41d9fbae3aa5005
Shivansh Singhal <shivansh@pop-os.localdomain>
2022-02-10 23:54:54 +0530

    Added class for NaaS rest client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,44 @@
-import { withWalletConnect, withMetamask } from "./AuthUtils/WalletAuth";
+import fetch from "node-fetch";
+import { withWalletConnect, withMetamask, ErrorHandler } from "./AuthUtils/WalletAuth";
+
+const DJANGO_BASE_URL = "https://api.onec.in/api/v1/naas/"
+
+class naas {
+    constructor(api_key) {
+        this.api_key = api_key;
+    }
+
+    async mintNFT(data) {
+        const url = `${DJANGO_BASE_URL}mintNFT/`
+        return fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+                "NAAS-APIKEY": this.api_key
+            }
+        }).then(async response => {
+            const mintRes = await response.json()
+            return mintRes
+        })
+        // const mintRes = await axios.post(url, data, {
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "NAAS-APIKEY": this.api_key
+        //     }
+        // })
+        // .then((response) => {
+        //     console.log(response)
+        //     return response;
+        // })
+
+        // return mintRes
+    }
+
+    verifyKey() {
+        return this.api_key
+    }
+}
 
 const auth = {
     withMetamask,
@@ -6,5 +46,6 @@ const auth = {
 }
 
 export default {
-    auth
+    auth,
+    naas
 }
